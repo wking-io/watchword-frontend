7cc0197479618662c0a7a77893c8af89e463f194
Will King <contact@wking.io>
2018-04-12 09:20:01 -0500

    Added asset import

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Elm from './app/Main.elm';
 import './main.scss';
 
+import './assets/images/cardback.jpg';
+
 const mountNode = document.getElementById('app');
 const app = Elm.Main.embed(mountNode);
 
 if (module.hot) {
-    module.hot.accept();
+  module.hot.accept();
 }
