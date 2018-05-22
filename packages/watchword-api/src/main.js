05f1673bf442c8d62c8cce1286571ae465477823
Will King <contact@wking.io>
2018-05-22 13:00:12 -0500

    Adding config

--- packages/watchword-api/src/main.js
+++ packages/watchword-api/src/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
 const Query = require('./resolvers/Query');
@@ -7,29 +9,29 @@ const AuthPayload = require('./resolvers/AuthPayload');
 const resolvers = {
   Query,
   Mutation,
-  AuthPayload,
+  AuthPayload
 };
 
 const server = new GraphQLServer({
   typeDefs: 'src/schema.graphql',
   resolvers,
   resolverValidationOptions: {
-    requireResolversForResolveType: false,
+    requireResolversForResolveType: false
   },
-  context: req => ({
-    ...req,
-    db: new Prisma({
-      typeDefs: 'src/generated/prisma.graphql',
-      endpoint: 'https://us1.prisma.sh/contact/watchword/dev',
-    }),
-  }),
+  context: req =>
+    Object.assign(req, {
+      db: new Prisma({
+        typeDefs: 'src/generated/prisma.graphql',
+        endpoint: 'https://us1.prisma.sh/contact/watchword/dev'
+      })
+    })
 });
 
 const options = {
   port: 8000,
   endpoint: '/graphql',
   subscriptions: '/subscriptions',
-  playground: '/playground',
+  playground: '/playground'
 };
 
 server.start(options, ({ port }) =>
