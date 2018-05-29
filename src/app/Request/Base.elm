cb3bd1d62bf6268f8f98c55e96203c08a28f5d79
Will King <contact@wking.io>
2018-05-29 13:28:13 -0500

    Updated API url to new now dev url

--- src/app/Request/Base.elm
+++ src/app/Request/Base.elm
@@ -9,6 +9,6 @@ import Task exposing (Task)
 makeRequest : SelectionSet decodesTo RootQuery -> Task (Graphqelm.Http.Error decodesTo) decodesTo
 makeRequest query =
     query
-        |> Graphqelm.Http.queryRequest "http://localhost:8000/graphql"
+        |> Graphqelm.Http.queryRequest "https://watchword-api.now.sh/graphql"
         |> Graphqelm.Http.withHeader "authorization" "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjamhodXUyaHA1c2o1MGI2MjJtcmVjcWtjIiwiaWF0IjoxNTI3MDA0MTMzfQ.k615sHbHSzgU7nE90ue5iql8x1Z63Ua8mvzj_wwkr6s"
         |> Graphqelm.Http.toTask
